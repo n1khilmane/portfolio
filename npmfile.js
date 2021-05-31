712946f609d94b87aa15c6669cc59217e61afb7d
nikhil_mane <nikhilmane1919@gmail.com>
2021-05-31 19:58:40 +0530

    sponsor changes

--- npmfile.js
+++ npmfile.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-    console.log("Visit http://photography.ramswaroop.me for a treat!");
+    console.log("Visit https://n1khilmane.github.io/portfolio/ for a treat!");
 };
